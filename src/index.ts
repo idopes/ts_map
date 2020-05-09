4dbec0eb87c657a468ceb3afb54aa25f53c14c59
IdoSamet <idosamet1@gmail.com>
2020-05-10 00:55:53 +0300

    ts practice

---
+++ src/index.ts
@@ -0,0 +1,17 @@
+// import { User } from './User';
+// import { Company } from './Company';
+import { CustomMap } from './CustomMap';
+import { User } from './User';
+import { Company } from './Company';
+
+// const user = new User();
+// const company = new Company();
+
+// console.log(user);
+// console.log(company);
+
+const user = new User();
+const company = new Company();
+const customMap = new CustomMap('map');
+customMap.addMarker(user);
+customMap.addMarker(company);
